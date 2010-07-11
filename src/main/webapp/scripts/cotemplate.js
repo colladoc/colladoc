4fe2303765b0d4b4906c7b1d21bfc3488d24b18b
Petr Hosek <petrhosek@gmail.com>
2010-07-11 23:25:29 +0000

    Fixed editor behavior

--- src/main/webapp/scripts/cotemplate.js
+++ src/main/webapp/scripts/cotemplate.js
@@ -4,4 +4,16 @@ $(document).ready(function() {
             function() { $('a.edit', this).show() },
             function() { $('a.edit', this).hide() }
             );
+
+    var docAllSigs = $("#template .signature");
+
+    $('a.edit').click(function(event) {
+       event.stopPropagation();
+       var shortComment = $("+ p.shortcomment", this.parentNode)
+       var fullComment = $("~ div.fullcomment:hidden", shortComment);
+       if (fullComment) {
+            fullComment.slideDown(100);
+            shortComment.slideUp(100);
+       }
+    });
 })
