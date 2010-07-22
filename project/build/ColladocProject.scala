e9628a82b26bd01556b092e606da5b4368e6f78d
Petr Hosek <petrhosek@gmail.com>
2010-07-22 01:51:47 +0000

    Configuration updated, Lift 2.1 snapshots now used

--- project/build/ColladocProject.scala
+++ project/build/ColladocProject.scala
@@ -3,12 +3,15 @@ import sbt._
 class ColladocProject(info: ProjectInfo) extends DefaultWebProject(info) with WinstoneProject {
   val snapshots = ScalaToolsSnapshots
 
-  val lift = "net.liftweb" % "lift-core" % "2.0-scala280-SNAPSHOT" % "compile"
-  val h2 = "com.h2database" % "h2" % "1.2.134" % "runtime"
-  val servlet = "javax.servlet" % "servlet-api" % "2.5" % "provided"
-  val junit = "junit" % "junit" % "4.7" % "test"
-  val specs = "org.scala-tools.testing" % "specs_2.8.0.Beta1" % "1.6.3" % "test"
-  val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.24" % "test"
+  override def libraryDependencies = Set(
+    "net.liftweb" % "lift-webkit_2.8.0" % "2.1-SNAPSHOT" % "compile",
+    "net.liftweb" % "lift-mapper_2.8.0" % "2.1-SNAPSHOT" % "compile",
+    "net.liftweb" % "lift-widgets_2.8.0" % "2.1-SNAPSHOT" % "compile",
+    "com.h2database" % "h2" % "1.2.138" % "runtime",
+    "junit" % "junit" % "4.7" % "test->default",
+    "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5-SNAPSHOT" % "test->default",
+    "org.mortbay.jetty" % "jetty" % "6.1.24" % "test->default"
+  ) ++ super.libraryDependencies
 
   override def localScala = defineScala("2.8.0-local", ("scala" / "build" / "pack").asFile) :: Nil
 }
