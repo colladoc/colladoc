bca4c2ace7b6ccd39440a806ad8d9be15a1c239b
Miroslav Paskov <mp3610@imperial.ac.uk>
2011-02-02 19:26:48 +0000

    Revert "- added selenium & selenium remote dependencies"
    This reverts commit 5ec24f4d6db7b6228bb4dd602c3b843bc204e556.

--- project/build/ColladocProject.scala
+++ project/build/ColladocProject.scala
@@ -1,9 +1,6 @@
-import bryanjswift.selenium.SeleniumPlugin
 import sbt._
 
-class ColladocProject(info: ProjectInfo) extends DefaultWebProject(info)
-      with WinstoneProject
-      with SeleniumPlugin {
+class ColladocProject(info: ProjectInfo) extends DefaultWebProject(info) with WinstoneProject {
   val snapshots = ScalaToolsSnapshots
 
   val liftMapper = "net.liftweb" % "lift-mapper_2.8.1" % "2.2" % "compile"
@@ -13,8 +10,6 @@ class ColladocProject(info: ProjectInfo) extends DefaultWebProject(info)
   val postgresql = "postgresql" % "postgresql" % "8.4-701.jdbc4"
   val junit = "junit" % "junit" % "4.8.2" % "test->default"
   val specs = "org.scala-tools.testing" % "specs_2.8.1" % "1.6.6" % "test->default"
-  val selenium = "org.seleniumhq.selenium" % "selenium" % "2.0b1" % "test->default"
-  val seleniumRemote = "org.seleniumhq.selenium" % "selenium-remote-control" % "1.0.1" % "test->default"
 
   override def localScala = defineScala("2.8.1-local", ("scala" / "build" / "pack").asFile) :: Nil
 
--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -3,7 +3,4 @@ import sbt._
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val github = "GitHub" at "http://petrh.github.com/m2/"
   val winstone = "com.github.petrh" % "sbt-winstone-plugin" % "1.0-SNAPSHOT"
-
-  val bryanSwift = "Bryan Swift" at "http://repos.bryanjswift.com/maven2"
-  val seleniumServer = "bryanjswift" % "selenium-sbt" % "0.0.4"
 }
