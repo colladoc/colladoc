5ec24f4d6db7b6228bb4dd602c3b843bc204e556
Miroslav Paskov <mp3610@imperial.ac.uk>
2011-02-02 18:52:29 +0000

    - added selenium & selenium remote dependencies - added dependency to selenium-sbt, selenium-start now runnable from sbt  - recorded one UI test with Selenium, added RunIntegrationTests  (do restart SBT and run update after this update)

---
+++ src/test/scala/scala/tools/colladoc/IntegrationTests.scala
@@ -0,0 +1,39 @@
+package scala.tools.colladoc
+
+import junit.framework.Assert._
+import junit.framework.{Assert, TestCase}
+import com.thoughtworks.selenium.DefaultSelenium
+import org.junit.{After, Before, Test}
+
+///**
+// * Created by IntelliJ IDEA.
+// * User: paskov
+// * Date: 2/2/11
+// * Time: 2:11 PM
+// * To change this template use File | Settings | File Templates.
+// */
+
+class IntegrationTests extends TestCase {
+
+  var s:DefaultSelenium = null
+
+	@Before
+	override def setUp() {
+		s = new DefaultSelenium("localhost", 4444, """*firefox""", "http://localhost:8080/");
+		s.start();
+	}
+
+  @Test
+  def testSearchForExactObject() = {
+    s.open("/")
+		s.`type`("//input[@type='text']", "object Foopa")
+		s.selectFrame("template")
+
+		assertTrue(s.isTextPresent("class Barpa extends AnyRef"));
+	}
+
+  @After
+	override def tearDown() {
+		s.stop();
+	}
+}
