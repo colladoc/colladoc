fcf804203fcde61cea823d0b57fcebf1b1df8e0c
Miroslav Paskov <mp3610@imperial.ac.uk>
2011-02-03 00:12:41 +0000

    Revert "Revert "- added selenium & selenium remote dependencies""
    This reverts commit bca4c2ace7b6ccd39440a806ad8d9be15a1c239b.

--- project/build/ColladocProject.scala
+++ project/build/ColladocProject.scala
@@ -1,6 +1,9 @@
+import bryanjswift.selenium.SeleniumPlugin
 import sbt._
 
-class ColladocProject(info: ProjectInfo) extends DefaultWebProject(info) with WinstoneProject {
+class ColladocProject(info: ProjectInfo) extends DefaultWebProject(info)
+      with WinstoneProject
+      with SeleniumPlugin {
   val snapshots = ScalaToolsSnapshots
 
   val liftMapper = "net.liftweb" % "lift-mapper_2.8.1" % "2.2" % "compile"
@@ -10,6 +13,8 @@ class ColladocProject(info: ProjectInfo) extends DefaultWebProject(info) with Wi
   val postgresql = "postgresql" % "postgresql" % "8.4-701.jdbc4"
   val junit = "junit" % "junit" % "4.8.2" % "test->default"
   val specs = "org.scala-tools.testing" % "specs_2.8.1" % "1.6.6" % "test->default"
+  val selenium = "org.seleniumhq.selenium" % "selenium" % "2.0b1" % "test->default"
+  val seleniumRemote = "org.seleniumhq.selenium" % "selenium-remote-control" % "1.0.1" % "test->default"
 
   override def localScala = defineScala("2.8.1-local", ("scala" / "build" / "pack").asFile) :: Nil
 
