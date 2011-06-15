541c67277f702870f106985e91645e6f494fa01f
ignatov <sergey@ignatov.spb.su>
2011-06-15 18:09:00 +0400

    added ColladocBoot bootstrap class

---
+++ src/main/scala/scala/tools/colladoc/boot/ColladocBoot.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2011, Sergey Ignatov. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided
+ * that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and
+ *     the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ *     and the following disclaimer in the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COLLABORATIVE SCALADOC PROJECT ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COLLABORATIVE SCALADOC
+ * PROJECT OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package scala.tools.colladoc.boot
+
+import tools.colladoc.model.mapper.User
+
+/**
+ * Bootstrap class for Colladoc project.
+ * @author Sergey Ignatov
+ */
+object ColladocBoot {
+  def boot {
+    addDefaultAdmin
+  }
+
+  def addDefaultAdmin {
+    val name = "colladoc"
+    val pass = "colladoc"
+    val isSuperUser = true
+
+    if (User.count() == 0)
+      User.create.userName(name).password(pass).superUser(isSuperUser).save
+  }
+}
