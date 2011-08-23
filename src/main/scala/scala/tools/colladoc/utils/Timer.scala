47895b5f231b2f27124379796917c09c5d33c808
ignatov <sergey@ignatov.spb.su>
2011-08-24 00:46:19 +0400

    Timer cleanup

--- src/main/scala/scala/tools/colladoc/utils/Timer.scala
+++ src/main/scala/scala/tools/colladoc/utils/Timer.scala
@@ -1,21 +1,37 @@
+/*
+ * Copyright (c) 2011, Rumyana Neykova. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided
+ * that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and
+ *     the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ *     and the following disclaimer in the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COLLABORATIVE SCALADOC PROJECT ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COLLABORATIVE SCALADOC
+ * PROJECT OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package scala.tools.colladoc.utils
 
-/**
- * Created by IntelliJ IDEA.
- * User: rumi
- * Date: 24/02/11
- * Time: 22:04
- * To change this template use File | Settings | File Templates.
- */
-
 object Timer {
-  var start:Long = 0L
-  var end:Long = 0L
-  def go = {
+  var start: Long = 0
+  var end: Long = 0
+
+  def go() {
     start = System.currentTimeMillis
   }
-  def stop = {
+
+  def stop() {
     end = System.currentTimeMillis
-    println(">   " + (end - start)/ 1000.0 + " s")
+    println(">   " + (end - start) / 1000.0 + " s")
   }
 }
